e170e51cf36b7f90024dd54127b02f3be0154ad7
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-08-07 19:30:04 -0500

    update

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -38,7 +38,7 @@ const Profile = () => {
   return (
     <div>
       <div className="flex-row justify-center mb-3">
-        <h2 className="col-12 col-md-10 bg-danger text-light p-3 mb-5">
+        <h2 className="col-12 col-md-10 bg-primary text-light p-3 mb-5">
           Viewing {userParam ? `${user.username}'s` : 'your'} comment.
         </h2>
 
@@ -52,8 +52,8 @@ const Profile = () => {
         </div>
         {!userParam && (
           <div
-            className="col-12 col-md-10 mb-3 p-3"
-            style={{ border: '1px dotted #1a1a1a' }}
+            className="bg-light col-12 col-md-10 mb-3 p-3"
+           // style={{ border: '1px dotted #1a1a1a' }}
           >
             <ThoughtForm />
           </div>
